26ef04c6df64db2a0ed4e032a090d90d75effa9d
kanondrago <ronaldchavezr@gmail.com>
2024-04-25 20:47:31 +0200

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import Header from './components/Header.vue'
+  import Formulario from './components/Formulario.vue';
+
+</script>
+
+<template>
+  <div class="container mx-auto mt-20">
+    <Header></Header>
+
+    <div class="mt-12 md:flex">
+
+      <Formulario></Formulario>
+    
+    </div>
+
+
+  </div>
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,38 @@
+<script setup>
+    
+</script>
+
+
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Seguimiento de pacientes</h2>
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade pacientes y 
+            <span class="text-indigo-600 font-bold">adminístralos</span>
+        </p>
+
+        <form 
+            class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+            action="">
+
+            <div class="mb-5">
+                <label 
+                    class="block text-gray-700 uppercase font-bold"
+                    for="mascota"
+                >
+                    Nombre mascota
+                </label>
+
+                <input 
+                    id="mascota"
+                    type="text"
+                    placeholder="Nombre de la mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                >
+            </div>
+
+
+
+        </form>
+    </div>
+</template>
